e1621207263d856e4fe4fe07c69a528710141f9e
Виноградова Светлана <me.basteg@gmail.com>
2017-05-07 20:07:40 +0400

    Логика

--- CrossZero/CrossZero.Lesson1/MainWindow.xaml.cs
+++ CrossZero/CrossZero.Lesson1/MainWindow.xaml.cs
@@ -23,6 +23,114 @@ namespace CrossZero.Lesson1
         public MainWindow()
         {
             InitializeComponent();
+            buttons = new []{ button11, button12, button13, button21, button22, button23, button31, button32, button33 };
+
+            foreach (var i in buttons)
+            {
+
+                i.Click += I_Click;
+            }
         }
+        string user = "";
+        string computer = "";
+        Button[] buttons;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Button[] buttons = { button11, button12, button13, button21, button22, button23, button31, button32, button33 };
+            foreach (var i in buttons)
+            {
+                i.IsEnabled = true;
+                i.Content = "";
+            }
+            
+
+            if (checkO.IsChecked == true && checkX.IsChecked != true)
+            {
+                ComputerPlay();
+            }
+
+            else if (checkO.IsChecked == checkX.IsChecked)
+            { MessageBox.Show("Выберите маркер"); }
+
+
+        }
+
+
+        private bool Wins()
+        {
+            if (((string)button11.Content != "" && button11.Content == button12.Content && button11.Content == button13.Content)
+                || ((string)button11.Content != "" && button11.Content == button21.Content && button11.Content == button31.Content)
+                || ((string)button12.Content != "" && button12.Content == button22.Content && button12.Content == button32.Content)
+                || ((string)button13.Content != "" && button13.Content == button23.Content && button13.Content == button33.Content)
+                || ((string)button21.Content != "" && button21.Content == button22.Content && button21.Content == button23.Content)
+                || ((string)button31.Content != "" && button31.Content == button32.Content && button31.Content == button33.Content)
+                || ((string)button11.Content != "" && button11.Content == button22.Content && button11.Content == button33.Content)
+                || ((string)button13.Content != "" && button13.Content == button22.Content && button13.Content == button31.Content))
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        private void EndGame()
+        {
+            foreach (var j in buttons)
+            {
+                j.IsEnabled = false;
+            }
+        }
+
+        private void I_Click(object sender, RoutedEventArgs e)
+        {
+            ((Button)sender).Content = user;
+
+            if (Wins())
+            {
+                MessageBox.Show("Вы выиграли");
+                EndGame();
+                
+            }
+            else ComputerPlay();
+        }
+
+        private void CheckX_Checked(object sender, RoutedEventArgs e)
+        {
+            this.user = "X";
+            this.computer = "O";
+
+        }
+
+        private void CheckO_Checked(object sender, RoutedEventArgs e)
+        {
+            this.user = "O";
+            this.computer = "X";
+        }
+
+        private void ComputerPlay()
+        {
+            foreach (var i in buttons)
+            {
+                if ((string)i.Content == "")
+                {
+                    i.Content = this.computer;
+                    i.IsEnabled = false;
+                    if (Wins())
+                    {
+                        MessageBox.Show("Вы проиграли");
+                        EndGame();
+                    }
+                    return;
+                }
+            }
+
+            MessageBox.Show("Ничья!");
+            EndGame();
+                
+            }
+        }
+
+
+
     }
-}
+
